d9d8aef19cab001a3567e87845ef7e58e94b6436
Nilesh Chaudhari <cnilec@gmail.com>
2024-02-04 14:01:20 +0530

    updated TODO

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@
 //Import the functions you need from the SDKs you need -->
         import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
         import { getDatabase, ref, get, query, onValue, child } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js"
-        //TODO: Add SDKs for Firebase products that you want to use https://firebase.google.com/docs/web/setup#available-libraries
+
 
         //Your web app's Firebase configuration
         const firebaseConfig = {
